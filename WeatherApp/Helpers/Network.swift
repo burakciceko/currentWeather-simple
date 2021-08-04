effd07fdba24b85dd0fc4aa34486325e19dacbdc
Burak Çiçek <burakcicek@Burak-MBP.lan>
2021-08-04 14:01:56 +0300

    key fixed

--- WeatherApp/Helpers/Network.swift
+++ WeatherApp/Helpers/Network.swift
@@ -20,7 +20,7 @@ class Network {
     let URL_BASE = "https://api.openweathermap.org/data/2.5"
     var API_URL_ADDED = ""
     
-    //var API_URL = "https://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.118092&units=metric&appid=2506e75f9bb46d287e2d40b8e239ffa5"
+    //var API_URL = "https://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.118092&units=metric&appid="
     
     func buildURL() -> String {
         API_URL_ADDED = "/weather?lat=" + URL_LATITUDE + "&lon=" + URL_LONGITUDE + "&units=metric" + "&appid=" + API_KEY
